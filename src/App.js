7cf0d785a58270d07bfeeda1c993f55de736d88a
SATYAM1548 <sbindroo_b20@ce.vjti.ac.in>
2022-01-15 04:40:40 +0530

    Pokedex game done

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import Pokegame from './Pokegame';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Pokegame />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Pokedex.js
@@ -0,0 +1,29 @@
+import { getTemplateInstallPackage } from 'create-react-app/createReactApp';
+import React, { Component } from 'react';
+import Pokecard from './Pokecard';
+import './Pokedex.css';
+
+class Pokedex extends Component {
+    render() {
+        let title;
+        if (this.props.isWinner) {
+            title = <h1 className="Pokedex-winner">Winning Hand</h1>
+        } else {
+            title = <h1 className="Pokedex-loser">Losing Hand</h1>
+        }
+        return (
+            <div className='Pokedex'>
+                <h1>Pokedex!</h1>
+                <p>Total Experience:{this.props.exp}</p>
+                {title}
+                <div className='Pokedex-cards'>
+                    {this.props.pokemon.map((p) => (
+                        <Pokecard id={p.id} name={p.name} type={p.type} base_exp={p.base_experience} />
+                    ))}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Pokedex;
